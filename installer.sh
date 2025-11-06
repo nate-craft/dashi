55bc75bafd5211b6817669bfeb475328ecb7e17f
Nate <secondarynate@icloud.com>
2025-11-06 16:15:01 -0500

    bump dashi installer script

--- installer.sh
+++ installer.sh
@@ -7,17 +7,17 @@ panic() {
 
 git clone https://github.com/nate-craft/dashi dashi || panic "Could not clone dashi repository!"
 
-cd dashi
+cd dashi || panic "Could not enter dashi repository!"
 
 sudo groupadd -f wheel
 sudo usermod -aG wheel "$USER"
 
 cat pkg/90-backlight.rules | sudo tee /etc/udev/rules.d/90-backlight.rules > /dev/null 2>&1 \
-    || panic "Could not install backlight udev rule"
+    || panic "Could not install backlight udev rule!"
 cat pkg/30-bluetooth.rules | sudo tee /etc/polkit-1/rules.d/30-bluetooth.rules > /dev/null 1>&1 \
-    || panic "Could not install bluetooth polkit rule"
+    || panic "Could not install bluetooth polkit rule!"
 
-cargo install --path . || panic "Could not install dashi"
+cargo install --path . || panic "Could not install dashi!"
 
 rm -rf dashi
 
